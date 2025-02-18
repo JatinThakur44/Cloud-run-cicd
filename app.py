91de6819d7b9815ea39fd4eaf8a38ac0c63833e0
JatinThakur44 <jatinthakur9193@gmail.com>
2025-02-18 12:26:53 +0000

    Updated GitHub Actions workflow for Cloud Run deployment

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Cloud Run with GitHub Actions!"
+    return "Hello , Cloud Run with GitHub Actions!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
